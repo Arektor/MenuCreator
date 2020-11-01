446d11b7946d6e9dfec5560fe97e4f586511ba5a
Arektor <maxence@symfonye.com>
2020-11-01 19:05:27 +0100

    commit signature test

--- src/main/java/fr/arektor/menucreator/commands/CMD_MenuCreator.java
+++ src/main/java/fr/arektor/menucreator/commands/CMD_MenuCreator.java
@@ -11,26 +11,28 @@ public class CMD_MenuCreator {
 
 	public static boolean handle(CommandSender sender, String label, String[] args) {
 		if (args.length >= 1) {
-			if (args[0].equalsIgnoreCase("config")) handleConfigSubcmd(sender, Arrays.copyOfRange(args, 1, args.length));
-			if (args[0].equalsIgnoreCase("create")) handleCreateSubcmd(sender, Arrays.copyOfRange(args, 1, args.length));
+			if (args[0].equalsIgnoreCase("config")) handleConfigSubcmd(sender, label, Arrays.copyOfRange(args, 1, args.length));
+			if (args[0].equalsIgnoreCase("create")) handleCreateSubcmd(sender, label, Arrays.copyOfRange(args, 1, args.length));
 		} else {
-			//TODO print help?
+			sender.sendMessage(ChatColor.AQUA + "MenuCreator commands:");
+			sender.sendMessage(ChatColor.DARK_BLUE + " - " + ChatColor.BLUE + " config <open/reload>");
+			sender.sendMessage(ChatColor.DARK_BLUE + " - " + ChatColor.BLUE + " create [internal name]");
 		}
 		return true;
 	}
 	
-	private static void handleConfigSubcmd(CommandSender sender, String[] args) {
+	private static void handleConfigSubcmd(CommandSender sender, String label, String[] args) {
 		if (args.length >= 1) {
 			if (args[0].equalsIgnoreCase("reload")) {
 				Config.reload();
 				sender.sendMessage(ChatColor.GREEN+"MenuCreator's config successfully reloaded.");
 			}
 		} else {
-			//TODO print help
+			sender.sendMessage(ChatColor.RED + "/"+label+" config <open/reload>");
 		}
 	}
 
-	private static void handleCreateSubcmd(CommandSender sender, String[] args) {
+	private static void handleCreateSubcmd(CommandSender sender, String label, String[] args) {
 		
 	}
 }
